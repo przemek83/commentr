76743ce37270e1b1f7e40b58a2b26effecf69420
przemek83 <4788832+przemek83@users.noreply.github.com>
2025-04-08 16:56:10 +0200

    Refactor ProxyStyle class to use nullptr.

--- src/ProxyStyle.h
+++ src/ProxyStyle.h
@@ -6,23 +6,20 @@
 class ProxyStyle : public QProxyStyle
 {
 public:
-    ProxyStyle(QString &name);
+    ProxyStyle(QString& name);
 
-    int pixelMetric(PixelMetric metric,
-                    const QStyleOption* option,
+    int pixelMetric(PixelMetric metric, const QStyleOption* option,
                     const QWidget* widget) const override;
 
-    QSize sizeFromContents(ContentsType type,
-                           const QStyleOption* option,
+    QSize sizeFromContents(ContentsType type, const QStyleOption* option,
                            const QSize& contentsSize,
-                           const QWidget* widget = 0) const override;
+                           const QWidget* widget = nullptr) const override;
 
     static void updateUisize();
 
-    void drawPrimitive(PrimitiveElement element,
-                       const QStyleOption* option,
+    void drawPrimitive(PrimitiveElement element, const QStyleOption* option,
                        QPainter* painter,
-                       const QWidget* widget = 0) const override;
+                       const QWidget* widget = nullptr) const override;
 
 private:
     Q_DISABLE_COPY(ProxyStyle)
@@ -30,4 +27,4 @@ private:
     static float actualUisize_;
 };
 
-#endif // PROXYSTYLE_H
+#endif  // PROXYSTYLE_H
